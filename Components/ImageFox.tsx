65094107c192ebe29a487836c1ef1c03cec8b151
PC1 <kevsalomdev@gmail.com>
2023-11-18 13:00:32 -0400

    lazy load

--- Components/ImageFox.tsx
+++ Components/ImageFox.tsx
@@ -1,23 +1,46 @@
-import type { FunctionComponent, FC } from "react"
-
+import { useRef, useEffect, useState } from "react";
 
 type Props = {
-  imagen:string
-}
+  imagen: string;
+};
 
 // Forma explícita "JSX.Element"
-export const ImageFox = ({imagen}:Props): JSX.Element => {
+export const ImageFox = ({ imagen }: Props): JSX.Element => {
+  const node = useRef<HTMLImageElement>(null);
+  const backGroound: string =
+    "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4=";
+  const [src, setSrc] = useState(backGroound);
 
-  
-	return (
-		<div>
-			<img src={imagen} width={450} className="rounded-md"/>
-		</div>
-	)
-}
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setSrc(imagen);
+        }
+      });
+    });
 
+    if (node.current) {
+      observer.observe(node.current);
+    }
 
+    return () => {
+      observer.disconnect;
+    };
+  }, []);
 
+  return (
+    <div>
+      <img
+        src={src}
+        ref={node}
+        width={450}
+        height="auto"
+        className="rounded-md bg-slate-100"
+      />
+    </div>
+  );
+};
 
 // // Forma implícito (JSX:Element)**
 // const Implicito = () => {
