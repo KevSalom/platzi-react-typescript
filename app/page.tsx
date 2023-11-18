b6a9492aeb2df07eb25d754ed6a7064d6cab0ce3
PC1 <kevsalomdev@gmail.com>
2023-11-18 12:01:20 -0400

    Tipado de eventos y handlers

--- app/page.tsx
+++ app/page.tsx
@@ -3,21 +3,30 @@ import { ImageFox } from "@/Components/ImageFox"
 import { useState } from "react"
 
 const random = (): number => Math.floor(Math.random()*123)+1
+const generateId = (): string => {
+  return (
+    Math.random().toString(36).substring(2, 15) +
+    Math.random().toString(36).substring(2, 15)
+  );
+};
 
+type ImageItem = {id:string, url:string}
 export default function Home() {
 
-  const [images, setImages] = useState<string[]>([
-    `https://randomfox.ca/images/${random()}.jpg`,
-    `https://randomfox.ca/images/${random()}.jpg`,
-    `https://randomfox.ca/images/${random()}.jpg`,
-    `https://randomfox.ca/images/${random()}.jpg`,
-    `https://randomfox.ca/images/${random()}.jpg`
-  ])
+  const [images, setImages] = useState<Array<ImageItem>>([])
 
+
+  const handleNewFox:React.MouseEventHandler<HTMLButtonElement> = ()=>{
+    const newImageItem:ImageItem = {id:generateId(), url:`https://randomfox.ca/images/${random()}.jpg`}
+    setImages([
+      ...images, newImageItem
+    ])
+  }
   return (
     <main>
       <h1 className="underline font-bold text-slate-500" >Hola Platzi =)</h1>
-      {images.map((image, index) => <div key={index} className="p-4"><ImageFox  imagen={image}/></div>)}
+      <button onClick={handleNewFox} className="bg-sky-600 rounded-md p-2">Agragar un nuevo Zorro</button>
+      {images.map(({url, id}) => <div key={id} className="p-4"><ImageFox  imagen={url}/></div>)}
     </main>
   )
 }
